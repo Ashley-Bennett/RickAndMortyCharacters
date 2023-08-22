623786417b43e474f519794cb230d6432266b2df
Ashley Bennett <ashleyben21@gmail.com>
2023-08-22 13:54:49 +0100

    basic data fetching

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import SearchForm from '@/components/SearchForm.vue'
+import DataFetcher from '@/components/DataFetcher.vue'
 </script>
 
 <template>
   <main>
     <SearchForm />
+    <DataFetcher />
   </main>
 </template>
 
---
+++ src/components/DataFetcher.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+const characterData = reactive({})
+
+function fetchData(charcterName: string) {
+  fetch('https://rickandmortyapi.com/api/character/?name=rick&page=1')
+    .then((response) => {
+      if (response.ok) {
+        console.log(response)
+        return response.json()
+      }
+      throw response
+    })
+    .then((data) => {
+      console.log(data)
+    })
+    .catch((error) => {
+      console.error('Error fetching data: ', error)
+    })
+    .finally(() => {
+      // loading logic
+    })
+}
+</script>
+<template lang="">
+  <button @click="fetchData">get ricks</button>
+</template>
+<style lang=""></style>
