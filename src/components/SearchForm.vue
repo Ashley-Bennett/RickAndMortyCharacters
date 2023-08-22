514e1a1bf4f89276f1a265dedf4e96675988ca76
Ashley Bennett <ashleyben21@gmail.com>
2023-08-22 13:34:42 +0100

    Add basic search bar and tidy repo

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const searchTerm = ref('')
+
+function handleSubmit() {
+  console.log(searchTerm.value)
+}
+</script>
+
+<template lang="">
+  <form @submit.prevent="handleSubmit">
+    <input v-model="searchTerm" type="text" />
+    <button type="submit">Search</button>
+  </form>
+</template>
+
+<style scoped></style>
